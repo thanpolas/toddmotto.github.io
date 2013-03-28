8570ca248c53e0728df867dc6ee613a63d74385c
Todd Motto <create@toddmotto.com>
2013-03-28 18:19:53 +0000

    New social buttons

--- js/scripts.js
+++ js/scripts.js
@@ -85,15 +85,15 @@ z.async = z.src = '//engine.carbonads.com/z/20676/azcarbon_2_1_0_HORIZDARK';
 var s = document.getElementsByTagName('script')[0];
 s.parentNode.insertBefore(z, s);
 
-// Twitter
+// Twitter Follow
 !function(d,s,id){
 	var js,fjs=d.getElementsByTagName(s)[0];
 	if(!d.getElementById(id)){js=d.createElement(s);js.id=id;
-	js.src="//platform.twitter.com/widgets.js";
+	js.src='//platform.twitter.com/widgets.js';
 	fjs.parentNode.insertBefore(js,fjs);
-}}(document,"script","twitter-wjs");
+}}(document,'script','twitter-wjs');
 
-// Facebook SDK
+// Facebook Like
 (function(d, s, id) {
 	var js, fjs = d.getElementsByTagName(s)[0];
 	if (d.getElementById(id)) return;
@@ -101,3 +101,22 @@ s.parentNode.insertBefore(z, s);
 	js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=252993844735607";
 	fjs.parentNode.insertBefore(js, fjs);
 })(document, 'script', 'facebook-jssdk');
+
+// Sharing: Facebook, Twitter, Google+
+(function(doc,s) {
+    var js, fjs = doc.getElementsByTagName(s)[0],
+        frag = doc.createDocumentFragment(),
+        add = function (url, id) {
+            if (doc.getElementById(id)) {
+                return;
+            }
+            js = doc.createElement(s);
+            js.src = url;
+            id && (js.id = id);
+            frag.appendChild(js);
+        };
+    add('http://connect.facebook.net/en_US/all.js#xfbml=1','facebook-jssdk');
+    add('http://platform.twitter.com/widgets.js');
+    add('https://apis.google.com/js/plusone.js');
+    fjs.parentNode.insertBefore(frag, fjs);
+})(document,'script');
