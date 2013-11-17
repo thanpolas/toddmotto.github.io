3176d0155650afb1d535cca9ed24fd27ad58faae
Todd Motto <toddmotto@todds-mbp.default>
2013-11-17 17:04:37 +0000

    Test

--- labs/echo/js/echo.js
+++ labs/echo/js/echo.js
@@ -21,8 +21,9 @@ window.Echo = (function (window, document, undefined) {
       var self = store[i];
       if (_inView(self)) {
         self.src = self.getAttribute('data-echo');
-        if ([].indexOf && store.indexOf(self) !== -1) {
-          [].slice.call(store).splice(i, 1);
+        if ([].indexOf && [].slice.call(store).indexOf(self) !== -1) {
+            [].slice.call(store).splice(i, 1);
+          }
         }
       }
     }
