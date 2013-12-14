4b3e6f40f325ce6d7909d1f04841196f7ff36de2
Todd Motto <todd@toddmotto.com>
2013-12-14 15:10:08 +0000

    Update fluidvids.js

--- labs/fluidvids/js/fluidvids.js
+++ labs/fluidvids/js/fluidvids.js
@@ -36,7 +36,7 @@ window.Fluidvids = (function (window, document, undefined) {
   var _appendStyles = function () {
     var CSS = '.fluidvids-elem{position:absolute;top:0px;left:0px;width:100%;height:100%;}.fluidvids{width:100%;position:relative;}';
     var htmDiv = document.createElement('div');
-    htmDiv.innerHTML = '<p>x</p><style>'+CSS+'</style>';
+    htmDiv.innerHTML = '<p></p><style>'+CSS+'</style>';
     head.appendChild(htmDiv.childNodes[1]);
   
 
